a9ffa55786ffbd570fa9fbe53f68453074eb89a8
Vincent <vj.smit2@gmail.com>
2016-04-21 17:14:20 +0200

    HAPI af

---
+++ code/rma1.cc
@@ -0,0 +1,77 @@
+using namespace std;
+
+sc_trace_file *tfHapi;
+
+class top : public ProcessNetwork {
+  public:
+  top(sc_module_name name) : ProcessNetwork(name) {
+    HAPI::TimingInformation fdsTask0(
+      2.5,      
+      2.5,      
+      SC_NS,   
+      1        
+    );
+
+    HAPI::TimingInformation fdsTask1(
+      4,      
+      4,      
+      SC_NS, 
+      1        
+    );
+
+    int bufferSize0 = 1;
+    CircularBuffer<void*> *fifo0= new CircularBuffer<void*>("Fifo0", 
+                                                      bufferSize0, 1, 1);
+    int bufferSize1 = 1;
+    CircularBuffer<void*> *fifo1= new CircularBuffer<void*>("Fifo1",  
+                                                      bufferSize1, 1, 1);
+
+    double src0Period = 5;
+    double src0FiringDuration = 0;
+    DefaultSensorTask* src0 = new DefaultSensorTask("source0", tfHapi, 
+                                                      src0FiringDuration, SC_NS);
+    src0->setPeriod(sc_time(src0Period, SC_NS));
+
+    double src1Period = 9;
+    double src1FiringDuration = 0;
+    double src1setInitialDelay = 0;
+    DefaultSensorTask* src1 = new DefaultSensorTask("source1", tfHapi, 
+                                                      src1FiringDuration, SC_NS);
+    src1->setPeriod(sc_time(src1Period, SC_NS));
+    src1->setInitialDelay(sc_time(src1setInitialDelay, SC_NS));
+    
+    // the higher the number the higher the priority
+    int priority0 = 1;
+    int priority1 = 0;
+    Processor* p = new Processor("proc", Processor::SCHED_FIXED_PRIORITY_PREEMPTIVE, 
+                                            tfHapi);
+    DefaultProcessorTask* t0 = new DefaultProcessorTask("task0", tfHapi, 
+                                                          fdsTask0, priority0, 0);
+    DefaultProcessorTask* t1 = new DefaultProcessorTask("task1", tfHapi, 
+                                                          fdsTask1, priority1, 0);
+
+    t0->enablePrintResponseTime();
+    t1->enablePrintResponseTime();
+
+    p->addTask(t0);
+    p->addTask(t1);
+
+    src0->addOutputPort()->bind(*fifo0);
+    t0->addInputPort()->bind(*fifo0);
+    src1->addOutputPort()->bind(*fifo1);
+    t1->addInputPort()->bind(*fifo1);
+
+    init_PN();
+  };
+};
+
+int sc_main(int argc , char *argv[]) {
+  tfHapi = sc_create_vcd_trace_file (argv[0]); // creates rma.vcd
+
+  top top1("Top1");
+
+//      sc_start(); // start simulation until stopped by  the user
+  sc_start(100000, SC_NS); // start simulation for 100 ns
+  sc_close_vcd_trace_file(tfHapi);
+  return 0;
+}
